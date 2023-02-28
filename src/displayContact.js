64fb94b76a24f98bbb7c7783781699b9fbd02872
ollyjw <owilcocks@gmail.com>
2023-02-28 11:53:31 +0000

    added contact page

--- src/displayContact.js
+++ src/displayContact.js
@@ -1,10 +1,34 @@
 function displayContact() {
     const content = document.getElementById('content');
 
-    const text = document.createElement('p');
-    text.textContent = 'ohyoyoyo';
+    const contactContainer = document.createElement('div');
+    contactContainer.classList.add('content-box');
 
-    content.appendChild(text);
+    const title = document.createElement('h1');
+    title.textContent = 'Get in touch';
+
+    contactContainer.appendChild(title);
+
+    const contactItem = (label, link, anchorText) => {
+        const div = document.createElement('div');
+        div.classList.add('contact-items');
+        div.textContent = `${label}:`;
+
+        const a = document.createElement('a');
+        a.classList.add('contact-link');
+        a.textContent = anchorText;
+        a.href = link;
+        div.appendChild(a);
+
+        contactContainer.appendChild(div);
+    }
+
+    contactItem('Email', 'mailto:fakerestaurantemail@email.com', 'fakerestaurantemail@email.com');
+    contactItem('Tel', 'tel:0123456789', '0123456789');
+    contactItem('Github', 'https://github.com/ollyjw', 'https://github.com/ollyjw');
+
+
+    content.appendChild(contactContainer);
 }
 
 export default displayContact;
--- src/displayHome.js
+++ src/displayHome.js
@@ -9,7 +9,7 @@ function displayHome() {
     const img = document.createElement('img');
     const desc = document.createElement('p');
 
-    title.textContent = 'Welcome to da restaurant zone';
+    title.textContent = 'Welcome to... this particular restaurant';
     img.src = 'https://www.cheatsheet.com/wp-content/uploads/2014/07/REST-e1405539646312.png';
 
     desc.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem in dignissimos tenetur vitae quasi odio possimus, necessitatibus earum velit veniam esse blanditiis alias nisi sapiente quaerat omnis eos ullam assumenda?';
